7ee0909199c611de6d9a6a1716c2d679311dda07
Aman <rumman.djuice1@gmail.com>
2019-02-19 02:39:10 +0100

    "commit"

--- src/App.js
+++ src/App.js
@@ -10,11 +10,20 @@ class App extends Component {
       {name:"Tansen", age:29}
     ]
   };
-  SwitchNameHandler=()=>{
+  SwitchNameHandler=(newName)=>{
     this.setState({
       persons:[
-        {name:"Aman Azim", age:32},
+        {name:newName, age:32},
         {name:"Roza Azim", age:24},
+        {name:"Tansen Khan", age:30}
+      ]
+    });
+  };
+  ChangeNameHandler=(event)=>{
+    this.setState({
+      persons:[
+        {name:"Aman", age:32},
+        {name:event.target.value, age:24},
         {name:"Tansen Khan", age:30}
       ]
     });
@@ -24,17 +33,19 @@ class App extends Component {
       <div className="App">
        <h1>Persons Info</h1>
 
-        <button onClick={this.SwitchNameHandler}>Switch Name</button>
+        <button onClick={()=>this.SwitchNameHandler('Azim')}>Switch Name</button>
 
         <Person name={this.state.persons[0].name}
                 age={this.state.persons[0].age}
-                clickToChange={this.SwitchNameHandler}/>
+                clickToChange={this.SwitchNameHandler.bind(this, 'Rumman-from child comp')}/>
 
         <Person name={this.state.persons[1].name}
                 age={this.state.persons[1].age}
-                clickToChange={this.SwitchNameHandler}>My Hobby: Playing</Person>
+                clickToChange={this.SwitchNameHandler.bind(this, 'Rumman-from child2 comp')}>My Hobby: Playing</Person>
 
-        <Person name="Tansen" age="29" clickToChange={this.SwitchNameHandler}/>
+        <Person name="Tansen"
+                age="29"
+                changed={this.ChangeNameHandler}/>
       </div>
     );
     //return React.createElement('div', {className:'App'}, React.createElement('h1', null,'Hi, I\'m a React App'));
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import './Person.css'
 
 const person=(props)=>{
+   const buttonStyle={
+        backgroundColor:'white',
+        font:'inherit',
+        border:'1px solid blue',
+        padding:'8px',
+       cursor:'pointer'
+   };
    return(
-       <div>
+       <div >
            <p>I am {props.name} and I am {props.age} years old</p>
            <p>{props.children}</p>
-           <button onClick={props.clickToChange}>Click to change name from child</button>
+           <button style={buttonStyle} onClick={props.clickToChange}>Click to change name from child</button>
+           <br></br>
+           <input type="text" onChange={props.changed} value={props.name}/>
        </div>
    );
 };
